981377efd9972778317495272eed0be54ec0ef9a
LSatan <hacker_oli@web.de>
2020-06-01 15:12:39 +0200

    description corrected

--- examples/Transmit_button_eeprom/Transmit_button_eeprom.ino
+++ examples/Transmit_button_eeprom/Transmit_button_eeprom.ino
@@ -29,10 +29,10 @@ simu.enableTransmit(6);
 //Remote ID1 and ID2 must be the same for every remote control.
 //If you want to use different remote IDs, you have to use a different remote number.
 //An eeprom of 512 bytes of memory can store up to 36 remote controls with 4 buttons each.
-simu.add_remote(0,0,60,160,40,0,0); // add the 1th button (Begin of Rolling code 0 = 0, Begin of Rolling code 1 = 0, Button ID, Remote ID1 = 60, Remote_ID2, Store to place(Remote number) = 0, Button number = 0)
-simu.add_remote(0,0,61,160,40,0,1); // add the 2th button to remote 0. 
-simu.add_remote(0,0,62,160,40,0,2); // add the 3th button to remote 0.
-simu.add_remote(0,0,63,160,40,0,3); // add the 4th button to remote 0.
+simu.add_remote(0,0,60,160,40,0,0); // add the 1th button to remote 0. (Begin of Rolling code0 = 0, Begin of Rolling code1 = 0, Button ID = 60, Remote ID1 = 160, Remote_ID2 = 40, Store to place(Remote number) = 0, Button number = 0)
+simu.add_remote(0,0,61,160,40,0,1); // add the 2th button to remote 0. (Begin of Rolling code0 = 0, Begin of Rolling code1 = 0, Button ID = 61, Remote ID1 = 160, Remote_ID2 = 40, Store to place(Remote number) = 0, Button number = 1)
+simu.add_remote(0,0,62,160,40,0,2); // add the 3th button to remote 0. (Begin of Rolling code0 = 0, Begin of Rolling code1 = 0, Button ID = 62, Remote ID1 = 160, Remote_ID2 = 40, Store to place(Remote number) = 0, Button number = 2)
+simu.add_remote(0,0,63,160,40,0,3); // add the 4th button to remote 0. (Begin of Rolling code0 = 0, Begin of Rolling code1 = 0, Button ID = 63, Remote ID1 = 160, Remote_ID2 = 40, Store to place(Remote number) = 0, Button number = 3)
 }
 
 void loop(){
